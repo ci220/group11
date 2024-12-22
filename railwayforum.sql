6e1e74dd489c9a775a5a305fd1e3268db5f3f126
ainmuharam <muharamnurain@gmail.com>
2024-12-22 18:48:31 +0800

    Update railwayforum.sql

--- railwayforum.sql
+++ railwayforum.sql
@@ -34,6 +34,7 @@ CREATE TABLE `users` (
   `username` varchar(50) NOT NULL,
   `email` varchar(100) NOT NULL,
   `password` varchar(255) NOT NULL,
+  `profile_picture` varchar(255) DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
