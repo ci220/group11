5521e4773a55e776cc62f63402a176452aa3b337
zhafs105 <ai220142@student.uthm.edu.my>
2024-12-25 19:22:30 +0800

    Update navigation.php

--- navigation.php
+++ navigation.php
@@ -9,6 +9,7 @@ if (session_status() === PHP_SESSION_NONE) {
         <?php if (isset($_SESSION['user_id'])): ?>
             <ul class="nav-links">
                 <li><a href="index.php">Home</a></li>
+                <li><a href="about_us.php">About Us</a></li>
                 <li><a href="messages.php">Messages</a></li>
                 <li><a href="userprofile.php">Profile</a></li>
                 <li><a href="chat.php">Chat</a></li>
@@ -18,6 +19,7 @@ if (session_status() === PHP_SESSION_NONE) {
         <?php else: ?>
             <ul class="nav-links">
                 <li><a href="index.php">Home</a></li>
+                <li><a href="about_us.php">About Us</a></li>
                 <li><a href="forums.php">Forums</a></li>
                 <li><a href="login.php">Login</a></li>
                 <li><a href="FAQ.php">FAQ</a></li>
